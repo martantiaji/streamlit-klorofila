333a3241d53c537ea3b31e3e09705463c6a2cc17
Martanti <martantiaji@gmail.com>
2022-05-17 02:21:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,47 +1,50 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-from apps import home, heatmap, upload  # import your app modules here
+from apps import (home,geemap_script)
+#import your app modules here
 
-st.set_page_config(page_title="Streamlit Geospatial", layout="wide")
+st.set_page_config(
+    page_title="Chlorophyll-a",
+    page_icon=":earth_asia:",
+    layout="wide",
+    initial_sidebar_state="auto",
+    menu_items=None)
 
 # A dictionary of apps in the format of {"App title": "App icon"}
 # More icons can be found here: https://icons.getbootstrap.com
 
 apps = [
     {"func": home.app, "title": "Home", "icon": "house"},
-    {"func": heatmap.app, "title": "Heatmap", "icon": "map"},
-    {"func": upload.app, "title": "Upload", "icon": "cloud-upload"},
+    {"func": geemap_script.app, "title": "Klorofil-a", "icon": "map"},
 ]
 
 titles = [app["title"] for app in apps]
-titles_lower = [title.lower() for title in titles]
 icons = [app["icon"] for app in apps]
 
 params = st.experimental_get_query_params()
 
 if "page" in params:
-    default_index = int(titles_lower.index(params["page"][0].lower()))
+    default_index = int(titles.index(params["page"][0].lower()))
 else:
     default_index = 0
 
 with st.sidebar:
     selected = option_menu(
-        "Main Menu",
+        "Menu Utama",
         options=titles,
         icons=icons,
         menu_icon="cast",
         default_index=default_index,
     )
 
-    st.sidebar.title("About")
+    st.sidebar.title("Tentang")
     st.sidebar.info(
         """
-        This web [app](https://share.streamlit.io/giswqs/streamlit-template) is maintained by [Qiusheng Wu](https://wetlands.io). You can follow me on social media:
-            [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://www.youtube.com/c/QiushengWu) | [LinkedIn](https://www.linkedin.com/in/qiushengwu).
+        Aplikasi web ini dikelola ole Martanti Aji dengan dosen pembimbing Dr. Lalu Muhamad Jaelani S.T., M.Sc., PhD. 
+        Url aplikasi web ini yaitu <https://streamlit.geemap.org>
         
-        Source code: <https://github.com/giswqs/streamlit-template>
+        Anda dapat mengakses referensi kode : [GitHub Aji](https://github.com/martantiaji/chlplandsat8.git)
 
-        More menu icons: <https://icons.getbootstrap.com>
     """
     )
 
