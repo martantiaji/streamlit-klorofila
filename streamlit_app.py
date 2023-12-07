6f22a80800586c21054f393b093a07972bdab80d
Martanti <martantiaji@gmail.com>
2023-12-07 10:32:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
+from streamlit-option-menu import option_menu
 from apps import (home,geemap_script)
 #import your app modules here
 
